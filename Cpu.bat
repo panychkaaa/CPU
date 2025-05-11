1b25e0333c37c28f49dab82234b3bd44207ceee6
panychkaaa <134150531+panychkaaa@users.noreply.github.com>
2025-05-11 12:06:31 +0500

    Add files via upload

---
+++ Cpu.bat
@@ -0,0 +1,43 @@
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFEPP 0
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFEPP1 0
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR LATENCYHINTPERF 0
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR LATENCYHINTPERF1 0
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR LATENCYHINTEPP 0
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR LATENCYHINTEPP1 0
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFAUTONOMOUSWINDOW 0
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR CPMINCORES 100
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR CPMINCORES1 100
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR CPMAXCORES 100
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR CPMAXCORES1 100
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR IDLEDEMOTE 100
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR IDLEPROMOTE 100
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PROCTHROTTLEMAX 100
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PROCTHROTTLEMAX1 100
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PROCTHROTTLEMIN 100
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PROCTHROTTLEMIN1 100
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFINCPOL 2
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFINCPOL1 2
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFINCTHRESHOLD 100
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFINCTHRESHOLD1 100
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFINCTIME 0
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFINCTIME1 0
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFDECPOL 1
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFDECPOL1 1
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFDECTHRESHOLD 100
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFDECTHRESHOLD1 100
+
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFDECTIME 0
+powercfg /setACvalueindex scheme_current SUB_PROCESSOR PERFDECTIME1 0
